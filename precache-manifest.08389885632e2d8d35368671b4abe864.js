e9fecaf0b4fcc04c14d40c83b93c6bfc07c840dd
Rishi-Raj-Kalita <rishirajkalita13@gmail.com>
2020-05-22 00:57:53 +0530

    Updates

---
+++ precache-manifest.08389885632e2d8d35368671b4abe864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066f60b6ad5b7f435fb2ba8570c2c3f7",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/index.html"
+  },
+  {
+    "revision": "cabfd530ef83fffc9eb9",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ef22279a37091903b08",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/static/js/2.e48b216c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/static/js/2.e48b216c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabfd530ef83fffc9eb9",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/static/js/main.22a91ba6.chunk.js"
+  },
+  {
+    "revision": "12103819f9c1fce5ebb1",
+    "url": "/https//Rishi-Raj-Kalita.github.io/Practice/static/js/runtime-main.547c730b.js"
+  }
+]);
